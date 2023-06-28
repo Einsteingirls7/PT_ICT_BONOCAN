f7338c9acab6a984caf4caa25a0415285b39238d
EinsteinGirls7 <128776225+Einsteingirls7@users.noreply.github.com>
2023-06-28 23:06:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ function sendMail(){
 	
 	};
 
-const serviceID ="service_1wkcb95";
-const templateID ="template_kyj5q4j";
-
+const serviceID ="service_yuevtph";
+const templateID ="service_yuevtph";
 emailjs.send(serviceID,templateID,params)
 .then(
 	res =>{
@@ -18,4 +17,4 @@ emailjs.send(serviceID,templateID,params)
 		console.log(res);
 		alert("Thank You Elsa Your Message For Me Has Been Sent Successfully");
 	})
-.catch(err=>console.log(err));
+.catch(err=>console.log(err));                                                       }
